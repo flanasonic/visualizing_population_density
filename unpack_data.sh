969ab44d5209e779e05bda3755a3717f6ce0c909
Julie Flanagan <flanasonic@gmail.com>
2022-03-01 19:43:02 -0500

    added thing to unpack_data.sh to create data folder if missing

--- unpack_data.sh
+++ unpack_data.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 THIS_DIR=$(dirname -- $(readlink -f ${BASH_SOURCE}) )
 
+if [ ! -d data ]
+then
+        echo "Data folder does not exist... creating"
+	mkdir -p ${THIS_DIR}/data
+else
+        echo "Data folder exists..."
+fi
+
 for f in  ${THIS_DIR}/data_archive/*.tgz
 do
   cp ${f} ${THIS_DIR}/data
