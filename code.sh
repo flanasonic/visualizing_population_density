a1dfcccf2a6bee6296ce8e13d1ad3e0b0c680e75
Julie Flanagan <flanasonic@gmail.com>
2022-03-01 21:07:42 -0500

    added notebooks and example of importing local modules

--- code.sh
+++ code.sh
@@ -3,23 +3,32 @@
 THIS_DIR=$(dirname -- $(readlink -f ${BASH_SOURCE}) )
 if [ ! -d ${THIS_DIR}/venv ]
 then
-	echo "Creating Virtual Environment"
+	echo "Creating Virtual Environment..."
 	python -m venv venv
-	pip -r requirements.txt
 fi
 
 echo "Activating Virtual Environment..."
 
 if [[ "$OSTYPE" =~ ^msys ]]; then
-   ${THIS_DIR}/venv/Scripts/activate
-   export PATH=${THIS_DIR}/venv/Scripts:$PATH    
+   source ${THIS_DIR}/venv/Scripts/activate
 else
    source ${VENV_SCRIPT}/venv/bin/activate
-   export PATH=${THIS_DIR}/venv/bin:$PATH
 fi
-export VIRTUAL_ENV
 
+echo "Installing required packages..."
+python -m pip install --disable-pip-version-check -q -r "${THIS_DIR}/requirements.txt"
 
-echo "Staring Visual Studio Code"
+echo "Staring Visual Studio Code..."
 
 code ./
+
+echo " "
+echo "To run jupyter lab execute the following commands: "
+
+if [[ "$OSTYPE" =~ ^msys ]]; then
+   echo "  source ${THIS_DIR}/venv/Scripts/activate"
+else
+   echo "  source ${VENV_SCRIPT}/venv/bin/activate"
+fi
+
+echo "  jupyter lab"
---
+++ src/clean.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def hello():
+    print("Hello :-) ")
+
+if __name__ == "__main__":    
+    hello()
