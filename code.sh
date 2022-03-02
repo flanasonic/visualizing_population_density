85a2f5d18a6be05241ba5e9e94c846479afb808a
Julie Flanagan <flanasonic@gmail.com>
2022-03-01 20:21:01 -0500

    added code.sh and src dir

--- code.sh
+++ code.sh
@@ -1,18 +1,25 @@
+#!/bin/bash
+
 THIS_DIR=$(dirname -- $(readlink -f ${BASH_SOURCE}) )
 if [ ! -d ${THIS_DIR}/venv ]
 then
-	echo "creating virtual environment"
+	echo "Creating Virtual Environment"
 	python -m venv venv
 	pip -r requirements.txt
 fi
 
-VEN
+echo "Activating Virtual Environment..."
 
 if [[ "$OSTYPE" =~ ^msys ]]; then
    ${THIS_DIR}/venv/Scripts/activate
-    
+   export PATH=${THIS_DIR}/venv/Scripts:$PATH    
 else
    source ${VENV_SCRIPT}/venv/bin/activate
+   export PATH=${THIS_DIR}/venv/bin:$PATH
 fi
+export VIRTUAL_ENV
+
+
+echo "Staring Visual Studio Code"
 
 code ./
